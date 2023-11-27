8444ec90c6e00d273e5a43e43687cb30409bdce0
Rick <rick@faqta.nl>
2023-11-27 16:36:20 +0100

    introduced monospace for better overview

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -5,15 +5,16 @@ export default function Game() {
   const [gameMap, setGameMap] = useState<(string | undefined)[][]>([]);
   useEffect(() => {
     setGameMap(createMap(10, 10));
-    console.log(gameMap);
   }, []);
 
   return (
     <>
       <h1>Game </h1>
-      {gameMap.map((row) => (
-        <div>{row.join(" ")}</div>
-      ))}
+      <div style={{ fontFamily: "Courier New, monospace" }}>
+        {gameMap.map((row, i) => (
+          <div key={i}>{row.join(" ")}</div>
+        ))}
+      </div>
     </>
   );
 }
